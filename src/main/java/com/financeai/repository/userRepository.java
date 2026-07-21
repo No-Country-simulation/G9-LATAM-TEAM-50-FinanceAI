eb692159e635cc5d95cef615d594ce7368d80969
Alejandro Guzman <alejandrogg9751@gmail.com>
2026-07-21 14:55:06 -0500

    dtos

---
+++ src/main/java/com/financeai/repository/userRepository.java
@@ -0,0 +1,22 @@
+package com.financeai.repository;
+
+
+import com.financeai.entity.user;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.Optional;
+
+
+@Repository
+public interface userRepository extends JpaRepository<user, Long> {
+
+
+    Optional<user> findByEmail(String email);
+
+
+    boolean existsByEmail(String email);
+
+
+}
